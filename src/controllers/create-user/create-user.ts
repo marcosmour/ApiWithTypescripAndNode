c0046f64586888a759b18770071f58a72d9a1ad4
Marcos Moura <marcosmoura201@hotmail.com>
2023-09-24 12:00:03 -0300

    refactor: add controller helper functions

--- src/controllers/create-user/create-user.ts
+++ src/controllers/create-user/create-user.ts
@@ -1,46 +1,38 @@
 import { User } from "../../models/user";
+import { badRequest, created, serverError } from "../helpers";
 import { HttpRequest, HttpResponse, IController } from "../protocols";
 import { CreateUserParams, ICreateUserRepository } from "./protocols";
 
 import validator from "validator";
 
 export class CreateUserController implements IController {
-    constructor(private readonly createUserRepository: ICreateUserRepository) {}
+  constructor(private readonly createUserRepository: ICreateUserRepository) {}
 
-    async handle(httpRequest: HttpRequest<CreateUserParams>): Promise<HttpResponse<User>> {
-        try {
-            // verificar campos obrigatorios
-            const requiredFields = ["firstName", "lastName", "email", "password"];
-            for(const field of requiredFields) {
-                if(!httpRequest?.body?.[field as keyof CreateUserParams]?.length) {
-                    return {
-                        statusCode: 400,
-                        body: `Field ${field} is required`,
-                    }
-                }
-            }
-
-            // verificar se o email e valido
-            const emailIsValid = validator.isEmail(httpRequest.body!.email);
-            if(!emailIsValid) {
-                return {
-                    statusCode: 400,
-                    body: "E-mail is invalid"
-                }
-            }
-    
-            const user = await this.createUserRepository.createUser(httpRequest.body!)
-            return {
-                statusCode: 201,
-                body: user,
-            }
-
-        } catch (error) { 
-            return {
-                statusCode: 500,
-                body: "Something went wrong"
-            }
+  async handle(
+    httpRequest: HttpRequest<CreateUserParams>
+  ): Promise<HttpResponse<User | string>> {
+    try {
+      // verificar campos obrigatorios
+      const requiredFields = ["firstName", "lastName", "email", "password"];
+      for (const field of requiredFields) {
+        if (!httpRequest?.body?.[field as keyof CreateUserParams]?.length) {
+          return badRequest(`Field ${field} is required`);
         }
-    }
+      }
 
+      // verificar se o email e valido
+      const emailIsValid = validator.isEmail(httpRequest.body!.email);
+      if (!emailIsValid) {
+        return badRequest("E-mail is invalid");
+      }
+
+      const user = await this.createUserRepository.createUser(
+        httpRequest.body!
+      );
+
+      return created<User>(user);
+    } catch (error) {
+      return serverError()
+    }
+  }
 }
--- src/controllers/delete-user/delete-user.ts
+++ src/controllers/delete-user/delete-user.ts
@@ -1,33 +1,25 @@
 import { User } from "../../models/user";
+import { badRequest, ok, serverError } from "../helpers";
 import { HttpRequest, HttpResponse, IController } from "../protocols";
-import {  IDeleteUserRepository } from "./protocols";
+import { IDeleteUserRepository } from "./protocols";
 
 export class DeleteUserController implements IController {
-    constructor(private readonly deleteUserRepository: IDeleteUserRepository) {}
-    async handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<User>> {
-        try {
-            
-            const id = httpRequest?.params?.id;
+  constructor(private readonly deleteUserRepository: IDeleteUserRepository) {}
+  async handle(
+    httpRequest: HttpRequest<any>
+  ): Promise<HttpResponse<User | string>> {
+    try {
+      const id = httpRequest?.params?.id;
 
-            if(!id) {
-                return {
-                    statusCode: 400,
-                    body: "Missing User id"
-                }
-            }
+      if (!id) {
+        return badRequest("Missing User id");
+      }
 
-            const user = await this.deleteUserRepository.deleteUser(id);
-            
-            return {
-                statusCode: 200,
-                body: user,
-            }
-        } catch (error) {
-            return {
-                statusCode: 500,
-                body: "Something went wrong"
-            }
-        }
+      const user = await this.deleteUserRepository.deleteUser(id);
+
+      return ok<User>(user)
+    } catch (error) {
+      return serverError()
     }
-
+  }
 }
